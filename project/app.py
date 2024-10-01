e1b470a022b0fa374c91ca2ca57931ffc955849e
Prarthona Paul <p.paul@mail.utoronto.ca>
2024-09-30 20:24:45 -0400

    first test

--- project/app.py
+++ project/app.py
@@ -52,74 +52,9 @@ def login_required(f):
 
     return decorated_function
 
-
 @app.route("/")
-def index():
-    """Searches the database for entries, then displays them."""
-    entries = db.session.query(models.Post)
-    return render_template("index.html", entries=entries)
-
-
-@app.route("/add", methods=["POST"])
-def add_entry():
-    """Adds new post to the database."""
-    if not session.get("logged_in"):
-        abort(401)
-    new_entry = models.Post(request.form["title"], request.form["text"])
-    db.session.add(new_entry)
-    db.session.commit()
-    flash("New entry was successfully posted")
-    return redirect(url_for("index"))
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """User login/authentication/session management."""
-    error = None
-    if request.method == "POST":
-        if request.form["username"] != app.config["USERNAME"]:
-            error = "Invalid username"
-        elif request.form["password"] != app.config["PASSWORD"]:
-            error = "Invalid password"
-        else:
-            session["logged_in"] = True
-            flash("You were logged in")
-            return redirect(url_for("index"))
-    return render_template("login.html", error=error)
-
-
-@app.route("/logout")
-def logout():
-    """User logout/authentication/session management."""
-    session.pop("logged_in", None)
-    flash("You were logged out")
-    return redirect(url_for("index"))
-
-
-@app.route("/delete/<int:post_id>", methods=["GET"])
-@login_required
-def delete_entry(post_id):
-    """Deletes post from database."""
-    result = {"status": 0, "message": "Error"}
-    try:
-        new_id = post_id
-        db.session.query(models.Post).filter_by(id=new_id).delete()
-        db.session.commit()
-        result = {"status": 1, "message": "Post Deleted"}
-        flash("The entry was deleted.")
-    except Exception as e:
-        result = {"status": 0, "message": repr(e)}
-    return jsonify(result)
-
-
-@app.route("/search/", methods=["GET"])
-def search():
-    query = request.args.get("query")
-    entries = db.session.query(models.Post)
-    if query:
-        return render_template("search.html", entries=entries, query=query)
-    return render_template("search.html")
-
+def hello():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run()
